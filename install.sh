dbf7fd64caee1da28424e6227291a14e9ba226ed
Jamie Rowe <jamie@getqsic.com>
2024-06-24 11:04:32 +1000

    hypr, install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf ~/.dotfiles/wezterm ~/.config/wezterm
+ln -sf ~/.dotfiles/fish ~/.config/fish
+ln -sf ~/.dotfiles/omf ~/.config/omf
+ln -sf ~/.dotfiles/hypr ~/.config/hypr
+ln -sf ~/.dotfiles/neovim ~/.config/nvim
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
