caf7f446fb577ad20315f190fdc621f9553039eb
Jamie Rowe <jamie@getqsic.com>
2024-08-09 14:41:18 +1000

    idk

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,6 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.font = wezterm.font "JetBrainsMono Mono"
 config.color_scheme = "tokyonight_moon"
 
 config.hide_tab_bar_if_only_one_tab = true
